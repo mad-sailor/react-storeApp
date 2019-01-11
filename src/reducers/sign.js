3469e383f4dc445f4e2d88ccbfbae2f0d6fe854c
mad-sailor <wd17826807599@163.com>
2019-01-11 15:33:06 +0800

    修改登录

--- src/reducers/sign.js
+++ src/reducers/sign.js
@@ -1,8 +1,10 @@
 import * as types from '../constants/ActionTypes';
 
-const userInfo = ( state = { },action ) => {
+const userInfo = ( state = {},action ) => {
     switch(action.type){
         case types.LOGIN:
+            if(JSON.stringify(state) !== '{}')
+                state = {}
             return {
                 uid: action.uid,
                 uname: action.uname,
